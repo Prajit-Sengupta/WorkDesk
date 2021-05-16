5e642804687c0dc5262b7c9836f6c5dd943b4ef2
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2021-05-16 22:51:10 +0530

    deletetask

--- Front-end/src/components/pages/TaskList.js
+++ Front-end/src/components/pages/TaskList.js
@@ -1,11 +1,16 @@
 import '../../assets/css/Content.css';
 
 const TaskList = ({ tasks }) => {
+
+    const handleDelete = (e) => {
+        fetch(`http://localhost:8000/todo/task-delete/${e.target.id}`, { method: 'DELETE' })
+        .then(() => console.log('Delete successful'));
+    }
     return (
         <ul className='tasks'>
             { tasks.map( task => (
                 <li className="task">
-                    <p> {task.title} </p>
+                    <p> {task.title} <i className='fas fa-times-circle' onClick={handleDelete}></i> </p>
                 </li>
             ))}
         </ul>
